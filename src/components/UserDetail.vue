23b9774d6cfcafda6ca759c841c7f36ee3857c7c
theperfectfuel <robert@theperfectfuel.com>
2018-03-09 23:56:38 -0800

    adds age change communication between sibling components

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,14 +2,23 @@
     <div>
         <h3>User Detail</h3>
         <p>User Name: {{name}}</p>
+        <p>User Age: {{age}}</p>
+        <button @click="nameWasReset">Reset Name</button>
     </div>
 </template>
 
 <script>
 export default {
-  props: {
-      name: String
-  }
+    props: {
+        name: String,
+        age: Number
+    },
+    methods: {
+        nameWasReset() {
+            this.name = 'Robert';
+            this.$emit('nameWasReset', this.name);
+        }
+    }
 }
 </script>
 
--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -1,3 +1,29 @@
 <template>
-  <h3>User Edit</h3>
+  <div class="component">
+      <h3>User Edit</h3>
+      <p>Edit me</p>
+      <p>User age: {{age}}</p>
+      <button
+        @click="editAge">
+        Edit Age
+      </button>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['age'],
+  methods: {
+    editAge() {
+      this.age = 30;
+      this.$emit('ageWasEdited', this.age);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    background-color: lightgreen;
+  }
+</style>
